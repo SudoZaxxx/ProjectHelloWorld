65017fcd766a6d456aed1a226eba8c8c7f10e665
SudoZaxxx <zachunderwoo08@gmail.com>
2026-06-05 22:53:59 -0400

    Intial Setup + Finished Script

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+   public static void main(String[] args){
+   
+    printHello();
+}
+
+    public static void printHello(){
+        for (int i=0; i<5; i++){
+            System.out.println("Hello World");
+            }
+        }
+    }
